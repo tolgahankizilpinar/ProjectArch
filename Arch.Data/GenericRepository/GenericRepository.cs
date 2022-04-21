d6eca6eecd766e881767c82b56c98c5e2670df72
tolgahan kızılpınar <tolgahan.kizilpinar@hotmail.com>
2022-04-21 13:51:16 +0300

    IGenericRepository implemented

---
+++ Arch.Data/GenericRepository/GenericRepository.cs
@@ -0,0 +1,70 @@
+﻿using Arch.Data.Context;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Arch.Data.GenericRepository
+{
+    public class GenericRepository<T> : IGenericRepository<T> where T : class
+    {
+        private readonly ArchContext _context;
+        private readonly DbSet<T> _dbSet;
+
+        public GenericRepository(ArchContext context)
+        {
+            _context = context;
+            _dbSet = context.Set<T>();
+        }
+
+
+        public IQueryable<T> GetAll()
+        {
+            return _dbSet;
+        }
+
+        public IQueryable<T> GetAll(Expression<Func<T, bool>> predicate)
+        {
+            return _dbSet.Where(predicate);
+        }
+
+        public T GetById(object id)
+        {
+            return _dbSet.Find(id);
+        }
+
+        public T Get(Expression<Func<T, bool>> predicate)
+        {
+            return _dbSet.Where(predicate).SingleOrDefault();
+        }
+
+        public void Add(T entity)
+        {
+            _dbSet.Add(entity);
+        }
+
+        public void AddRange(List<T> entityList)
+        {
+            _dbSet.AddRange(entityList);
+        }
+
+        public void Update(T entity)
+        {
+            _dbSet.Attach(entity);
+            _context.Entry(entity).State = EntityState.Modified;
+        }
+
+        public void Delete(T entity)
+        {
+            if (_context.Entry(entity).State == EntityState.Detached)
+            {
+                _dbSet.Attach(entity);
+            }
+            _dbSet.Remove(entity);
+        }
+
+    }
+}
---
+++ Arch.Data/GenericRepository/IGenericRepository.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Arch.Data.GenericRepository
+{
+    public interface IGenericRepository<T> where T : class
+    {
+        IQueryable<T> GetAll();
+        IQueryable<T> GetAll(Expression<Func<T, bool>> predicate);  // get the all list according to where clause
+
+        T GetById(object id);
+        T Get(Expression<Func<T, bool>> predicate);
+
+        void Add(T entity);
+        void AddRange(List<T> entityList);
+
+        void Update(T entity);
+        void Delete(T entity);
+    }
+}
