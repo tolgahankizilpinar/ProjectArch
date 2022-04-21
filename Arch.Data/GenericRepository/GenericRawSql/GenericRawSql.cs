9611b44d6392a09d1e75af2ae42fda79e94b270a
tolgahan kızılpınar <tolgahan.kizilpinar@hotmail.com>
2022-04-21 20:37:42 +0300

    IGenericRawSql is implemented

---
+++ Arch.Data/GenericRepository/GenericRawSql/GenericRawSql.cs
@@ -0,0 +1,27 @@
+﻿using Arch.Data.Context;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Arch.Data.GenericRepository.GenericRawSql
+{
+    public class GenericRawSql : IGenericRawSql
+    {
+        private readonly CommonContext _context; 
+
+        public GenericRawSql(CommonContext context)
+        {
+            _context = context;
+        }
+
+        public DbRawSqlQuery Execute(string connstr, Type t, string sql, params object[] parameter)
+        {
+            _context.Database.Connection.ConnectionString = connstr;
+
+            return _context.Database.SqlQuery(t, sql, parameter);
+        }
+    }
+}
---
+++ Arch.Data/GenericRepository/GenericRawSql/IGenericRawSql.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Arch.Data.GenericRepository.GenericRawSql
+{
+    public interface IGenericRawSql
+    {
+        DbRawSqlQuery Execute(string connstr, Type t, string sql, params object[] parameter);
+    }
+}
